45617d0bc55aea74d66651fceb001cd16575acfa
Rafael Nicolaou <rnicolaouceng@gmail.com>
2026-02-26 22:26:22 +0200

    lesson: create components,create interfaces

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import type { HousingLocationInfo } from "../housinglocation";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+	selector: "app-home",
+	imports: [HousingLocationComponent],
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+	readonly baseUrl = "https://angular.dev/assets/images/tutorials/common";
+	housingLocation: HousingLocationInfo = {
+		id: 9999,
+		name: "Test Home",
+		city: "Test city",
+		state: "ST",
+		photo: `${this.baseUrl}/example-house.jpg`,
+		availableUnits: 99,
+		wifi: true,
+		laundry: false,
+	};
 }
